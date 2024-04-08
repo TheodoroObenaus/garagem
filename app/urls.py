2f3244a065fb9309ff8ec1dd6d7bbb7c56352f35
TheodoroObenaus <theodoro.obeanaus@gmail.com>
2024-04-08 11:44:55 -0300

    Criação Da APi Marca

--- app/urls.py
+++ app/urls.py
@@ -9,10 +9,13 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet, AcessorioViewSet
+from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet
 
 router = DefaultRouter()
 router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"Cores", CorViewSet)
+router.register(r"Marcas", MarcaViewSet)
 router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
